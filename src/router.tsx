e0130703648485d81d54f4a0ee4331dd8fac1039
Enzo Bozzani <bozzanienzo@gmail.com>
2023-10-01 21:46:11 -0300

    started project and created router

---
+++ src/router.tsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import { RootLayout } from "./components";
+import { Home } from "./pages";
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <RootLayout />,
+        children: [
+            {
+                index: true,
+                element: <Home />
+            }
+        ]
+    }
+]);
+
+export default router;
